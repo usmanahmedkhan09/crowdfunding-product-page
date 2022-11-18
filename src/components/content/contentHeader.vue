9b3d151017c70bbbfd227cdf1cd1ac7f20ad65c9
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-18 22:31:46 +0500

    changes in content header

--- src/components/content/contentHeader.vue
+++ src/components/content/contentHeader.vue
@@ -7,14 +7,24 @@
     <p class="subtitle">
       A beautiful & handcrafted moniter stand to reduce neck and eye strain
     </p>
+    <div class="buttons">
+      <button class="button">Back this project</button>
+      <div class="bookmarkBtn">
+        <div class="icon_wrapper">
+          <BookmarkIcon />
+        </div>
+        <span>BookMark</span>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import MastercraftLogo from "../icons/logo-mastercraft.vue";
+import BookmarkIcon from "../icons/bookmark.vue";
 
 export default defineComponent({
-  components: { MastercraftLogo },
+  components: { MastercraftLogo, BookmarkIcon },
   setup() {
     return {};
   },
