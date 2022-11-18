4258aca98b77d79a4631ae115b1aa5134a67c53d
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-18 20:54:12 +0500

    changes

--- src/components/content/contentHeader.vue
+++ src/components/content/contentHeader.vue
@@ -1,10 +1,20 @@
 <template>
-  <div class="contentHeader card"></div>
+  <div class="contentHeader card">
+    <div class="header_logo">
+      <MastercraftLogo />
+    </div>
+    <h2 class="title">Mastercraft Bamboo Monitor Riser</h2>
+    <p class="subtitle">
+      A beautiful & handcrafted moniter stand to reduce neck and eye strain
+    </p>
+  </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
+import MastercraftLogo from "../icons/logo-mastercraft.vue";
 
 export default defineComponent({
+  components: { MastercraftLogo },
   setup() {
     return {};
   },
