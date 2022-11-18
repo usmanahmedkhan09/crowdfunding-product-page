d7c9669081deb5451069c221660cd8f4beeda547
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-18 23:26:41 +0500

    adding slider

---
+++ src/components/content/contentStatics.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contentStatics card">
+    <div class="wrapper">
+      <div class="amount">
+        <h2>89,914</h2>
+        <span>of 100,000 backed</span>
+      </div>
+      <div class="backers">
+        <h2>5,007</h2>
+        <span>total backers</span>
+      </div>
+      <div class="days">
+        <h2>56</h2>
+        <span>days left</span>
+      </div>
+    </div>
+    <div class="input_range">
+      <input type="range" name="price" id="price" min="0" max="100" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/content/index.vue
+++ src/components/content/index.vue
@@ -1,14 +1,16 @@
 <template>
   <main class="content">
     <ContentHeaderVue />
+    <ContentStaticsVue />
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import ContentHeaderVue from "./contentHeader.vue";
+import ContentStaticsVue from "./contentStatics.vue";
 
 export default defineComponent({
-  components: { ContentHeaderVue },
+  components: { ContentHeaderVue, ContentStaticsVue },
   setup() {
     return {};
   },
