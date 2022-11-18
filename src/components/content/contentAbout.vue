dddeb061e159d9ab7a059969ca142a5d9f9b0b2a
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-19 00:53:37 +0500

    added content about section

---
+++ src/components/content/contentAbout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="contentAbout card">
+    <p class="title">About this project</p>
+    <p class="description">
+      The Matercarft Bamboo Mointer Riser is the study and stylish plateform
+      that elavates your screen to more comfortable viewing height.Placing your
+      mointer at eye level has the potentialto imrove your posture nad make you
+      more comfortable while at work, helping you stay focused at task at hand.
+    </p>
+    <p class="description">
+      Featuring artisan craftsmanship, the simplicity of design creates extra
+      desk space below your computer to allows notepads, pens and USB stoicks to
+      be stored under the stand.
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/components/content/index.vue
+++ src/components/content/index.vue
@@ -2,15 +2,16 @@
   <main class="content">
     <ContentHeaderVue />
     <ContentStaticsVue />
+    <ContentAbout />
   </main>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import ContentHeaderVue from "./contentHeader.vue";
 import ContentStaticsVue from "./contentStatics.vue";
-
+import ContentAbout from "./contentAbout.vue";
 export default defineComponent({
-  components: { ContentHeaderVue, ContentStaticsVue },
+  components: { ContentHeaderVue, ContentStaticsVue, ContentAbout },
   setup() {
     return {};
   },
