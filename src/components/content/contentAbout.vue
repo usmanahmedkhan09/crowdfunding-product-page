ec8a088ab2bcac3984b9713dc54514d4382b65d7
usman ahmed khan <usmanahmedkhan@live.com>
2022-11-19 17:44:06 +0500

    cards added for bamboo stand

--- src/components/content/contentAbout.vue
+++ src/components/content/contentAbout.vue
@@ -12,14 +12,53 @@
       desk space below your computer to allows notepads, pens and USB stoicks to
       be stored under the stand.
     </p>
+    <BambooCardVue
+      v-for="(stand, index) in cardDetails"
+      :key="index"
+      :data="stand"
+    ></BambooCardVue>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import BambooCardVue from "./resuable/bambooCard.vue";
 
 export default defineComponent({
+  components: { BambooCardVue },
   setup() {
-    return {};
+    const cardDetails = ref([
+      {
+        title: "Bamboo Stand",
+        priceTitle: "Pledge $25 or more",
+        price: 25,
+        left: 101,
+        btntext: "Select Reward",
+        description:
+          "You get an ergonomic stand made of natural bamboo.You've helped us launch our promotional campaign, and you'll be added to a special Backer member list",
+      },
+      {
+        title: "Black Edition Stand",
+        priceTitle: "Pledge $75 or more",
+        price: 75,
+        left: 64,
+        btntext: "Select Reward",
+        description:
+          "You get a Black Special Edition computer stand and a personal thank you.You'll be added to our Backer member list.Shipping is included",
+      },
+      {
+        title: "Mahogany Special Edition",
+        priceTitle: "Pledge $200 or more",
+        price: 75,
+        left: 0,
+        btntext: "Out of stock",
+        description:
+          "You get a two Specail Edition Mahogany stands, a Backer T-shirt, and personal thank you.You'll be added to our Backer member list.Shipping is included",
+      },
+    ]);
+
+    return {
+      cardDetails,
+    };
   },
 });
 </script>
---
+++ src/components/content/resuable/bambooCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="bambooCard card">
+    <div class="bambooCard--header">
+      <p class="title">{{ bambooStand.title }}</p>
+      <p class="price">{{ bambooStand.priceTitle }}</p>
+    </div>
+    <div class="bambooCard--body">
+      <p>{{ bambooStand.description }}</p>
+    </div>
+    <div class="bambooCard--footer">
+      <h1>
+        {{ bambooStand.left }}
+        <sup>left</sup>
+      </h1>
+
+      <button class="button">{{ bambooStand.btntext }}</button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    let bambooStand = ref(props.data);
+
+    return { bambooStand };
+  },
+});
+</script>
